3507c77d08485cb8fa0acf1bda598be7e1d03bbb
nWoWolfpac <minhduc5clhp@gmail.com>
2025-11-16 22:30:52 +0700

    Update Model

--- src/models/deeplabv3plus.py
+++ src/models/deeplabv3plus.py
@@ -13,7 +13,6 @@ class ASPP(nn.Module):
 
         self.blocks = nn.ModuleList()
 
-        # 1x1 conv
         self.blocks.append(
             nn.Sequential(
                 nn.Conv2d(in_channels, out_channels, 1, bias=False),
@@ -22,7 +21,6 @@ class ASPP(nn.Module):
             )
         )
 
-        # Dilated conv
         for r in rates:
             self.blocks.append(
                 nn.Sequential(
@@ -33,7 +31,6 @@ class ASPP(nn.Module):
                 )
             )
 
-        # Image pooling
         self.image_pool = nn.Sequential(
             nn.AdaptiveAvgPool2d(1),
             nn.Conv2d(in_channels, out_channels, 1, bias=False),
@@ -41,7 +38,6 @@ class ASPP(nn.Module):
             nn.ReLU(inplace=True)
         )
 
-        # Projection
         self.project = nn.Sequential(
             nn.Conv2d(out_channels * (len(rates) + 2), out_channels, 1, bias=False),
             nn.BatchNorm2d(out_channels),
@@ -51,6 +47,7 @@ class ASPP(nn.Module):
 
     def forward(self, x):
         h, w = x.shape[2:]
+
         res = [blk(x) for blk in self.blocks]
 
         img = self.image_pool(x)
@@ -94,53 +91,36 @@ class DeepLabV3Plus(nn.Module):
     def __init__(self, num_classes=21, pretrained=True):
         super().__init__()
 
-        # -----------------------
-        # MobileNetV3 backbone
-        # -----------------------
         backbone = mobilenet_v3_large(pretrained=pretrained)
 
-        # Low-level feature (stride=4) → 24 channels
-        self.low_level = backbone.features[0:3]
-
-        # High-level feature (stride=16) → 960 channels
-        self.high_level = backbone.features[3:]
+        self.low_level = backbone.features[0:3]     # stride /4
+        self.high_level = backbone.features[3:]     # stride /16
 
         self.low_level_channels = 24
         self.high_level_channels = 960
 
-        # Reduce low-level channels
         self.low_reduce = nn.Sequential(
             nn.Conv2d(self.low_level_channels, 48, 1, bias=False),
             nn.BatchNorm2d(48),
             nn.ReLU(inplace=True)
         )
 
-        # ASPP
         self.aspp = ASPP(self.high_level_channels, 256)
-
-        # Decoder
         self.decoder = DecoderBlock(256, 48, 256)
 
-        # Final classifier
         self.classifier = nn.Conv2d(256, num_classes, 1)
 
     def forward(self, x):
         h, w = x.shape[2:]
 
-        # Encoder
-        x_low = self.low_level(x)        # 1/4
-        x_high = self.high_level(x_low)  # 1/16
+        x_low = self.low_level(x)
+        x_high = self.high_level(x_low)
 
-        # ASPP
         x_aspp = self.aspp(x_high)
-
-        # Reduce low-level
         x_low_r = self.low_reduce(x_low)
 
-        # Decoder
         x_dec = self.decoder(x_aspp, x_low_r)
 
-        # Upsample
         out = F.interpolate(x_dec, size=(h, w), mode="bilinear", align_corners=False)
 
         return self.classifier(out)
